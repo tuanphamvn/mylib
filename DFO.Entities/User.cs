4c7aaec6c1653f268f32907fb8fb51150a1b450a
Tuan Pham <tuan.pham@dfo.global>
2019-01-16 15:57:51 +0700

    Add entity project

---
+++ DFO.Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DFO.Entities
+{
+    public class User : Entity
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+}
